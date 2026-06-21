4bd5f7c1f8df33d69df1c5d247bc1aa0a3ab6c60
김기표 <kipyo@gimgipyoui-MacBookAir.local>
2026-06-21 22:56:51 +0900

    Set VWorld deployment config

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 const config = {
   molitKey: "960e7b9c681010f60dfea81ec847bf36a664e995219611319701b453bf07433a",
   kakaoKey: "a706a5e96fa435d168f1dfc6a32d6fe1",
-  vworldKey: "",
-  vworldDomain: "",
+  vworldKey: "05C7C6D0-0CAA-368A-B0ED-B52E6EEDA8F7",
+  vworldDomain: "https://mybudongsan26.vercel.app",
 };
 
 module.exports = async function handler(req, res) {
